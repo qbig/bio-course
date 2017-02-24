37e1625874a74d51d57438c1c2b7569063e2050f
Qiao Liang <qiao.liang@grabtaxi.com>
2017-02-24 22:46:27 +0800

    added doc for line-> kmers

--- course1/str-compose.py
+++ course1/str-compose.py
@@ -6,6 +6,11 @@ l = int(lines[0])
 line = lines[1]
 
 def strCompose(line, length):
+	"""
+	Code Challenge: Solve the String Composition Problem.
+     Input: An integer k and a string Text.
+     Output: Compositionk(Text) (the k-mers can be provided in any order).
+	"""
 	return [line[i:i+length] for i in range(len(line)-length+1)]
 
 for i in strCompose(line, l):
